0d4da2585c3c94d12a607cd107caedcd73434600
cs572go <tushartushar572@gmail.com>
2024-01-26 19:53:00 +0530

    fundamental prompt edit

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ import google.generativeai as genai
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_fundamental_prompt(stock_name, exchange, prompt_data):
-	fundamental_prompt = f"Act as a stock market expert and perform fundamental analysis on the company {stock_name} listed on {exchange}. Assess the company's financial health by analyzing its most recent balance sheet, income statement, and cash flow statement. Evaluate its management effectiveness, competitive position in the industry, and future growth prospects. Provide insights into the company's metrics: {prompt_data}. Return the analysis in four paragraphs, each containing 80-100 words. Output Language: English."
+	fundamental_prompt = f"Act as a stock market expert and perform fundamental analysis on the company {stock_name} listed on {exchang}. Only provide insights into the company's metrics: {prompt_data}. Return the analysis in three paragraphs, each containing 80-100 words. Output Language: English."
 	return fundamental_prompt
 
 def get_technical_prompt(time_frame, stock_name, exchange):
