765a1b5fb70e14bb7e0dd9722ce736f7afaf4e96
ANIKET INAMDAR <aniketinamdar02@gmail.com>
2024-01-26 17:26:22 +0530

    Added function for fundamental_scraper

--- fundamental_scraper.py
+++ fundamental_scraper.py
@@ -1,34 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-ticker = "HDFC BANK"
-url = f'https://www.screener.in/company/{ticker}/consolidated/'
+def fundamental_scraper(stock_ticker):
+    url = f'https://www.screener.in/company/{stock_ticker}/consolidated/'
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        about = soup.find('div', class_='about')
+        # paragraphs = about.find_all('p') if about else []
+        # for p in paragraphs:
+        #     print(p.get_text(strip=True))
+        p = about.find('p')
+        # print(p.get_text(strip=True))
+        ratios = soup.find('div', class_='company-ratios')
+        data = {}
+        for li in ratios.find('ul', id='top-ratios').find_all('li'):
+            name = li.find('span', class_='name').text.strip()
+            value = li.find('span', class_='value').text.strip()
+            data[name] = value
+        del(data['Face Value'])
+        for key, value in data.items():
+            # print(f"{key}:{value}")
+            return p.get_text(strip=True), data
+    else:
+        print("Failed to retrieve the webpage")
 
-response = requests.get(url)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'html.parser')
-    about = soup.find('div', class_='about')
-    
-    # paragraphs = about.find_all('p') if about else []
-    # for p in paragraphs:
-    #     print(p.get_text(strip=True))
-
-    p = about.find('p')
-    print(p.get_text(strip=True))
-
-    ratios = soup.find('div', class_='company-ratios')
-    data = {}
-
-    for li in ratios.find('ul', id='top-ratios').find_all('li'):
-        name = li.find('span', class_='name').text.strip()
-        value = li.find('span', class_='value').text.strip()
-        data[name] = value
-
-    del(data['Face Value'])
-
-    for key, value in data.items():
-        print(f"{key}:{value}")
-
-else:
-    print("Failed to retrieve the webpage")
